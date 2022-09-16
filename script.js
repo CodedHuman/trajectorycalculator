a037dd79b7da0236d4f52fbbc3822bc0ac87b2f2
Sathvik G <sathvik1105@gmail.com>
2022-09-16 13:54:04 -0500

    fix

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 var input = document.getElementById('input').value;
-var output = document.getElementById('output');
+var output = document.getElementById('output').value;
+var calc = document.getElementById('calc');
 
 calc.onclick = function() {
-    output.value = Math.pow((input*0.2), 1.12);
+    output = Math.pow((input*0.2), 1.12);
 }
