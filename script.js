818dad60441987617d3436ac60d56edded5ceb7c
Sathvik G <sathvik1105@gmail.com>
2022-09-16 13:50:56 -0500

    button functionality and formulaic stuff

--- script.js
+++ script.js
@@ -1,1 +1,6 @@
+var input = document.getElementById('input').value;
+var output = document.getElementById('output');
 
+calc.onclick = function() {
+    output.value = Math.pow((input*0.2), 1.12);
+}
