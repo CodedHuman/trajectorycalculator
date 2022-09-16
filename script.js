0b54ba1f36fa78f3038169c071abd57365230907
Sathvik G <sathvik1105@gmail.com>
2022-09-16 14:14:37 -0500

    fix

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
-var input = document.getElementById('input').valueAsNumber;
+var input = parseInt(document.getElementById('input').value);
 
 function clickevent() {
-    document.getElementById('output').valueAsNumber = Math.pow((input*0.2), 1.12);
+    document.getElementById('output').value = Math.pow((input*0.2), 1.12);
 }
