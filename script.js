8d96a631656cf9f360b074572a4f300850d57055
Sathvik G <sathvik1105@gmail.com>
2022-09-16 14:04:26 -0500

    fix

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 var input = document.getElementById('input').value;
 
 function clickevent() {
-    document.getElementById('output').value = Math.pow((input*0.2), 1.12);
+    document.getElementById('output').value = Math.pow((input*0.2), 1.12).toString();
 }
